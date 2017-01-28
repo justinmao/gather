a4c7bf7fe0854ad144e68a1bffd251d9f0e50467
Justin Mao <justinmao@live.com>
2017-01-27 21:07:56 -0500

    test: added email presence/length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :owned_events, class_name: 'Event'
   has_and_belongs_to_many :events
   has_and_belongs_to_many :timeslots
+  validates :email, presence: true, length: { maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(
+      email: "testy@mctest.com",
+      first_name: "Testy",
+      last_name: "Jones",
+      password: "ENCRYPTME",
+      salt: "PEPPER")
+  end
+
+  test "unmodified user should be valid" do
+    assert @user.valid?
+  end
+
+  test "email should not be blank" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "x" * 245 + "@mctest.com"
+    assert_not @user.valid?
+  end
+
 end
