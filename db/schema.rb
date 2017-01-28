e2e0f5d59ffeb1cc7ba21544ea2f7f357778d305
Justin Mao <justinmao@live.com>
2017-01-27 22:05:13 -0500

    dev: added indexing to user emails /w uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128002723) do
+ActiveRecord::Schema.define(version: 20170128030305) do
 
   create_table "events", force: :cascade do |t|
     t.integer  "owner_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170128002723) do
     t.string   "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
